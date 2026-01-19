98a2d878b1124469ad98ab73fef517fc814a0299
Chris Lin <chrislin90210@gmail.com>
2026-01-19 14:02:47 -0600

    styling navbar

--- src/models/prepopulate.js
+++ src/models/prepopulate.js
@@ -1,83 +1,90 @@
-const fs = require("node:fs")
-const pool = require("./pool")
-const path = require("path")
-const bcrypt = require("bcryptjs")
-
+const fs = require("node:fs");
+const pool = require("./pool");
+const path = require("path");
+const bcrypt = require("bcryptjs");
 
 const users = [
     {
         name: "test 1",
         password: bcrypt.hashSync("123", 10),
-        username: "test@example.com",
+        username: "1@example.com",
         is_member: true,
         is_admin: true,
     },
     {
         name: "test 2",
         password: bcrypt.hashSync("456", 10),
-        username: "test2@example.com",
+        username: "2@example.com",
         is_member: true,
         is_admin: false,
     },
     {
         name: "test 3",
         password: bcrypt.hashSync("789", 10),
-        username: "test3@example.com",
+        username: "3@example.com",
         is_member: false,
         is_admin: false,
-    }
-]
+    },
+];
 
 const posts = [
     {
-        username: "test@example.com",
+        username: "1@example.com",
         title: "hello world",
         body: "my 1st post",
     },
     {
-        username: "test2@example.com",
+        username: "2@example.com",
         title: "1232*(&^*($#",
         body: "test",
-    }
-]
+    },
+];
 
 const insert_template = (table, mapping) => {
-    const cols = Object.keys(mapping).join(",")
-    const values = Object.values(mapping).map(val => typeof val === "string" ? `\'${val}\'` : val).join(", ")
+    const cols = Object.keys(mapping).join(",");
+    const values = Object.values(mapping)
+        .map((val) => (typeof val === "string" ? `\'${val}\'` : val))
+        .join(", ");
     return `
             INSERT INTO ${table} (${cols})
                 VALUES (${values}); 
-    `
-}
+    `;
+};
 
 const populate = async () => {
-    const conn = await pool.connect()
+    const conn = await pool.connect();
     try {
-        await conn.query("BEGIN")
-        const schema = fs.readFileSync(path.join(__dirname, "schema.sql"), "utf-8")
-        console.log(schema)
-        await conn.query(schema)
+        await conn.query("BEGIN");
+        const schema = fs.readFileSync(
+            path.join(__dirname, "schema.sql"),
+            "utf-8",
+        );
+        console.log(schema);
+        await conn.query(schema);
 
-        const user_query = users.map(user => insert_template("users", user)).join("\n")
+        const user_query = users
+            .map((user) => insert_template("users", user))
+            .join("\n");
 
-        console.log(user_query)
+        console.log(user_query);
 
-        await conn.query(user_query)
+        await conn.query(user_query);
 
-        const posts_query = posts.map(post => insert_template("posts", post)).join("\n")
+        const posts_query = posts
+            .map((post) => insert_template("posts", post))
+            .join("\n");
 
-        console.log(posts_query)
+        console.log(posts_query);
 
-        await conn.query(posts_query)
+        await conn.query(posts_query);
 
-        await conn.query("COMMIT")
+        await conn.query("COMMIT");
     } catch (err) {
-        console.error("something went wrong populating db", err)
-        await conn.query("ROLLBACK")
+        console.error("something went wrong populating db", err);
+        await conn.query("ROLLBACK");
     } finally {
-        conn.release()
+        conn.release();
     }
+};
 
-}
-
-populate()
+populate();
